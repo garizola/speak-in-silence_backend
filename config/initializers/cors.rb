d690cdaac4725621ec772aeb42c109f48df8dbfd
garizola <99741841+garizola@users.noreply.github.com>
2024-10-21 13:46:58 -0500

    db changes

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     # origins "localhost:3002"
-    origins "https://speak-in-silence.netlify.app"
+    origins "*"
 
     resource "*",
       headers: :any,
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.2].define(version: 2024_10_16_173223) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "todos", force: :cascade do |t|
     t.string "todo_name"
     t.boolean "completed"
